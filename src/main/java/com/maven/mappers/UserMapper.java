b0aa11dce164ad2215cda77fa4a17a97d14c235a
Balaji Sridharan <balaji.sridharan.ext@nokia.com>
2022-03-11 15:16:11 +0530

    Added Maven for Mapstructs

---
+++ src/main/java/com/maven/mappers/UserMapper.java
@@ -0,0 +1,17 @@
+package com.maven.mappers;
+
+import com.maven.model.User;
+import com.maven.pojo.UserDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    UserDTO userToUserDTO(User user);
+
+    User userDTOToUser(UserDTO userDTO);
+
+}
---
+++ src/main/java/com/maven/pojo/UserDTO.java
@@ -0,0 +1,18 @@
+package com.maven.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserDTO {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
