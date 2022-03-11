3cf1666bce95c7a868d1ff7d12931a45b52d6744
Balaji Sridharan <balaji.sridharan.ext@nokia.com>
2022-03-11 17:29:33 +0530

    Maven Surefire for JUnit4

---
+++ src/test/java/com/maven/example/HelloWorldTest.java
@@ -0,0 +1,11 @@
+package com.maven.example;
+
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+
+    @org.junit.Test
+    public void main() {
+        HelloWorld.main(null);
+    }
+}
