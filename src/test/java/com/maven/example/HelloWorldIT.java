0455f00650f04007d6d2ebb6885724f14018c80c
Balaji Sridharan <balaji.sridharan.ext@nokia.com>
2022-03-14 20:50:01 +0530

    Added Failsafe Plugin for Integration Tests cases

---
+++ src/test/java/com/maven/example/HelloWorldIT.java
@@ -0,0 +1,13 @@
+package com.maven.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloWorldIT {
+
+    @Test
+    void main() {
+        System.out.println("My IT Case");
+    }
+}
